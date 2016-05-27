933a1928a2d288799f94d5446a52a7d9d3b642b1
Mars Tan <davidtjx424@live.com>
2016-05-27 23:32:33 +0800

    Added data framework

--- src/cc/isotopestudio/ISOcentVIP/data/PlayerData.java
+++ src/cc/isotopestudio/ISOcentVIP/data/PlayerData.java
@@ -1,12 +1,56 @@
 package cc.isotopestudio.ISOcentVIP.data;
 
+import cc.isotopestudio.ISOcentVIP.type.VIPType;
+
+import java.util.List;
+
 /**
  * Created by Mars on 5/27/2016.
  * Copyright ISOTOPE Studio
  */
 public class PlayerData {
 
+    public static void addDays(String playerName, int days) {
+
+    }
+
     public static int getPoints(String playerName) {
         return -1;
+    }
+
+    public static int getLvl(String playerName) {
+        return -1;
+    }
+
+    public static int getLvlReqPoints(String playerName) {
+        return -1;
+    }
+
+    public static int getLvlReqPoints(int lvl) {
+        return -1;
+    }
+
+    public static int getCurrentPoints(String playerName) {
+        return -1;
+    }
+
+    public static int getNextLvlPoints(String playerName) {
+        return -1;
+    }
+
+    public static int getRemainDays(String playerName) {
+        return -1;
+    }
+
+    public static String getExpireDate(String playerName) {
+        return "";
+    }
+
+    public static VIPType getVIPType(String playerName) {
+        return VIPType.NONE;
+    }
+
+    public static List<String> getRank(int count){
+        return null;
     }
 }
---
+++ src/cc/isotopestudio/ISOcentVIP/type/VIPType.java
@@ -0,0 +1,9 @@
+package cc.isotopestudio.ISOcentVIP.type;
+
+/**
+ * Created by Mars on 5/27/2016.
+ * Copyright ISOTOPE Studio
+ */
+public enum VIPType {
+    mVIP, yVIP, NONE;
+}
